988e21695c5e73af9d9e2dbf3065eae81a1d9c6a
Sebastian Park <SebPark03@gmail.com>
2019-10-22 17:52:59 -0400

    Changed main.
    Changed main program to incorporate graphics.

--- GraphingCalculator/src/Main.java
+++ GraphingCalculator/src/Main.java
@@ -1,7 +1,90 @@
-public class Main {
+//Adapted from Basic Game Application from Mr. Chun
+
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main implements Runnable {
+
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
+
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public BufferStrategy bufferStrategy;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main ex = new Main();
+        new Thread(ex).start();
     }
-    
+
+    public Main() {
+
+        setUpGraphics();
+
+    }
+
+    public void run() {
+
+        while (true) {
+
+            moveThings();
+            render();
+            pause(20);
+        }
+    }
+
+
+    public void moveThings()
+    {
+
+    }
+
+    public void pause(int time ){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");
+
+        panel = (JPanel) frame.getContentPane();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        panel.setLayout(null);
+
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+
+    }
+
+
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        g.dispose();
+
+        bufferStrategy.show();
+    }
 }
