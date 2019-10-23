876da4442ccb90c3fd838740f2b3dcf42086d51a
Sebastian Park <SebPark03@gmail.com>
2019-10-23 16:44:42 -0400

    Add scaling and camera movement.
    Added a key listener and a mouse wheel listener to be able to make simple movements to control the width and position of the graph.

--- GraphingCalculator/src/Function.java
+++ GraphingCalculator/src/Function.java
@@ -7,37 +7,49 @@ import java.util.Arrays;
 public class Function {
     public double[][] points, transformedPoints;
     public int totalPoints;
-    Polygon graphicalFunction;
+//    Polygon graphicalFunction;
     public int[] xCo, yCo;//different ways of looking at all the coordinates; an array of all the x coordinates and an array of all the y
     public int xScale = 15, yScale = 1;
     public int xFocus, yFocus = 0;
 
     public Color functionColor;
 
-    public Function(int min, int max, double step) {
+    public Function(int min, int max, double precision) {
 
         functionColor = new Color(41, 196, 254);
 
         int counter = 0;
-        totalPoints = (int) ((max-min) / step);
+        totalPoints = (int) ((max-min) / precision);
         points = new double[totalPoints][2];
         transformedPoints = new double[totalPoints][2];
         xCo = new int[totalPoints];
         yCo = new int[totalPoints];
 
 
-        for (double i = min; i < max; i += step) {
+        for (double i = min; i < max; i += precision) {
 //            points[counter] = new double[]{i, Math.pow(i, 4) + 3 * Math.pow(i, 3) + 6 * i + 4};
-//            points[counter] = new double[]{i, (i-3)*(i-5)*(i-7)};
+            points[counter] = new double[]{i, (i-3)*(i-5)*(i-7)};
 //            points[counter] = new double[]{i, Math.pow(i,2)};
-            points[counter] = new double[]{i, 6*Math.pow(i,7) + 3*Math.pow(i,3) + 7};
+//            points[counter] = new double[]{i, 6*Math.pow(i,7) + 3*Math.pow(i,3) + 7};
             xCo[counter]= (int)(points[counter][0]*xScale + 500 + xFocus);
             yCo[counter]= (int)((points[counter][1])*-1+350+yFocus);
             counter++;
 //            System.out.println(Arrays.deepToString(points));
         }
 
-        graphicalFunction = new Polygon(xCo,yCo,totalPoints);
+//        graphicalFunction = new Polygon(xCo,yCo,totalPoints);
+
+    }
+
+    public void transform(int scaleXAxis, int scaleYAxis, int xCenter, int yCenter){
+        xScale = scaleXAxis;
+        yScale = scaleYAxis;
+        xFocus = xCenter;
+        yFocus = yCenter;
+        for (int i = 0; i < xCo.length; i++) {
+            xCo[i]= (int)(points[i][0]*xScale + 500 + xFocus);
+            yCo[i]= (int)((points[i][1])*-1*yScale+350+yFocus);
+        }
 
     }
 
--- GraphingCalculator/src/Main.java
+++ GraphingCalculator/src/Main.java
@@ -2,12 +2,13 @@
 
 
 import java.awt.Graphics2D;
+import java.awt.event.*;
 import java.awt.image.BufferStrategy;
 import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Main implements Runnable {
+public class Main implements Runnable, MouseListener, MouseWheelListener, MouseMotionListener, KeyListener {
 
     final int WIDTH = 1000;
     final int HEIGHT = 700;
@@ -22,6 +23,8 @@ public class Main implements Runnable {
 
     public int scale = 1;
 
+    public boolean isShift, up, down, left, right;
+
     public static void main(String[] args) {
         Main ex = new Main();
         new Thread(ex).start();
@@ -29,25 +32,44 @@ public class Main implements Runnable {
 
     public Main() {
 
-        mainFunction = new Function(-500,500,0.5);
+        mainFunction = new Function(-500,500,0.05);
         setUpGraphics();
-
+        canvas.addKeyListener(this);
+        canvas.addMouseListener(this);
+        canvas.addMouseMotionListener(this);
+        canvas.addMouseWheelListener(this);
+        canvas.setFocusable(true);
     }
 
     public void run() {
 
         while (true) {
 
-            moveThings();
+            scale();
             render();
             pause(20);
         }
     }
 
 
-    public void moveThings()
+    public void scale()
     {
-
+        if(up){
+            if(isShift) {
+                mainFunction.transform(mainFunction.xScale + 1, mainFunction.yScale, mainFunction.xFocus, mainFunction.yFocus);
+            }
+            else{
+                mainFunction.transform(mainFunction.xScale,  mainFunction.yScale, mainFunction.xFocus, mainFunction.yFocus+1);
+            }
+        }
+        if(down){
+            if(isShift) {
+                mainFunction.transform(mainFunction.xScale - 1,  mainFunction.yScale, mainFunction.xFocus, mainFunction.yFocus);
+            }
+            else{
+                mainFunction.transform(mainFunction.xScale,  mainFunction.yScale, mainFunction.xFocus, mainFunction.yFocus-1);
+            }
+        }
     }
 
     public void graph(){
@@ -91,7 +113,7 @@ public class Main implements Runnable {
     private void render() {
         Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
         g.clearRect(0, 0, WIDTH, HEIGHT);
-
+        g.setStroke(new BasicStroke(5));
         g.setColor(mainFunction.functionColor);
 //        g.drawPolygon(mainFunction.graphicalFunction);
         for (int i = 0; i < mainFunction.totalPoints-1; i++) {
@@ -105,5 +127,100 @@ public class Main implements Runnable {
         g.dispose();
 
         bufferStrategy.show();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        if(e.getWheelRotation()<-1){
+            up = true;
+            down = false;
+        }
+        if(e.getWheelRotation()>1){
+            down = true;
+            up = false;
+        }
+        if(e.getWheelRotation()>=-1&&e.getWheelRotation()<=1){
+            up = false;
+            down = false;
+        }
+        System.out.println(e.getWheelRotation());
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == 16){
+            isShift = true;
+        }
+        if(e.getKeyCode() == 37){
+            left = true;
+        }
+        if(e.getKeyCode() == 38){
+            up = true;
+        }
+        if(e.getKeyCode() == 39){
+            right = true;
+        }
+        if(e.getKeyCode() == 40){
+            down = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == 16){
+            isShift = false;
+        }
+        if(e.getKeyCode() == 37){
+            left = false;
+        }
+        if(e.getKeyCode() == 38){
+            up = false;
+        }
+        if(e.getKeyCode() == 39){
+            right = false;
+        }
+        if(e.getKeyCode() == 40){
+            down = false;
+        }
     }
 }
